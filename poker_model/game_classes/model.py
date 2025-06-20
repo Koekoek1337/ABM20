db7e81c58b550c19dadab46c176c0fee8e6a520f
Hajo Groen <hajo-groen@mail.com>
2025-06-20 17:47:02 +0200

    Fixed mesa port

--- poker_model/game_classes/model.py
+++ poker_model/game_classes/model.py
@@ -12,3 +12,11 @@ class spatialModel(mesa.Model):
         
     def tourney(self, agent):
         pass
+
+class IterativeCompetition(mesa.Model):
+    def __init__(self, nPlayers, agentUpdateMode="null", updateParms={"maxStepsize":0.1}, *args, 
+                 seed = None, rng = None, **kwargs):
+        super().__init__(*args, seed=seed, rng=rng, **kwargs)
+
+        AuctionPlayer.create_agents(self, nPlayers, updateMode=agentUpdateMode, updateParms=updateParms)
+        self.game = Game([player for player in self.agents], self.rng)
--- poker_model/runTest_poker.py
+++ poker_model/runTest_poker.py
@@ -1,20 +1,11 @@
 import numpy as np
 
+from game_classes.model import IterativeCompetition
 from game_classes.game import Game
 from game_classes.agents import AuctionPlayer
 from plotting.plotting import plotWealth
 
-rng = np.random.default_rng(42)
+model = IterativeCompetition(5, "null", {"maxStepsize": 0.1})
+data = model.game.play(1000, 10, False)
 
-nPlayers = 5
-
-players  = [AuctionPlayer(rng, "null") for _ in range(nPlayers)]
-for player in players[1:]:
-    player.set_strategy(players[0].getData()[0])
-
-game = Game(players, rng)
-
-data = game.play(1000, 100)
-
-print(data[0][0])
 plotWealth(data)
