0c7b063b1d3b154e536a20335ef8b4c5a9046265
Hajo <106412846+Koekoek1337@users.noreply.github.com>
2025-06-20 17:27:05 +0200

    file management

--- poker_model/__main__.py
+++ poker_model/__main__.py
@@ -1,1 +1,11 @@
-import game_classes
+from game_classes.model import spatialModel
+
+def main(): 
+    a = spatialModel(seed=42)
+    print(a)
+
+def test():
+    a = spatialModel
+
+if __name__ == "__main__":
+    main()
