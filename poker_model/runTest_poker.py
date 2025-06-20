b6123cadb41d481869fb6f8807349f6ee46e80e9
Hajo <106412846+Koekoek1337@users.noreply.github.com>
2025-06-20 17:27:34 +0200

    Changes for easyer testing

--- poker_model/runTest_poker.py
+++ poker_model/runTest_poker.py
@@ -2,16 +2,19 @@ import numpy as np
 
 from game_classes.game import Game
 from game_classes.agents import AuctionPlayer
+from plotting.plotting import plotWealth
 
 rng = np.random.default_rng(42)
 
-nPlayers = 2
+nPlayers = 5
 
 players  = [AuctionPlayer(rng, "null") for _ in range(nPlayers)]
-
-players[1].set_strategy(players[0].getData()[0])
-
+for player in players[1:]:
+    player.set_strategy(players[0].getData()[0])
 
 game = Game(players, rng)
 
-game.play(10, 10000)
+data = game.play(1000, 100)
+
+print(data[0][0])
+plotWealth(data)
